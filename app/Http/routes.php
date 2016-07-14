3948126c5699103552ef917232754065ee3a8c25
Pablo Lucas <pablo.lucv@gmail.com>
2016-07-14 12:24:02 -0500

    Routing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+# Vue
+Route::any('{all}', function () {
+   return view('index');
+})->where(['all' => '.*']);
--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import App from './components/App.vue';
+import HomeView from './components/HomeView.vue';
 
 // Install the router
 Vue.use(Router);
 
-var router = new Router();
+var router = new Router({
+    history: true
+});
+
+//Register component
+router.map({
+    '/': {
+        name: 'home',
+        component: HomeView
+    }
+});
+
+//Redirect doesn't exists pages
+router.redirect({
+    '*': '/'
+});
 
 router.start(App, 'app');
